30b78a5f3c22f547ee75f3deb73045649800aa88
Yusuke Ito <novi.mad@gmail.com>
2016-06-02 19:36:14 +0900

    add build script for libdispatch

---
+++ libdispatch/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=``"$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"``
+
+apt-get update && \
+    sudo apt-get install -y autoconf libtool libkqueue-dev libkqueue0 libcurl4-openssl-dev libbsd-dev libblocksruntime-dev && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+cd && \
+    git clone --recursive -b swift-3.0-preview-1-branch https://github.com/apple/swift-corelibs-libdispatch.git && \
+    pushd swift-corelibs-libdispatch
+
+patch -p0 < $SCRIPT_DIR/01-include-header.patch
+
+sh ./autogen.sh && \
+./configure --with-swift-toolchain=${SWIFT_ROOT}/usr --prefix=${SWIFT_ROOT}/usr && \
+make && make install && popd
